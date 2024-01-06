b2f1b601a1fbe3845bc76ee6734638303237da16
jazzjazzy <jsjazzau@gmail.com>
2024-01-06 17:02:29 +1100

    removed some files from the old system

--- src/routes/api/(shopDescriptions)/saveDesc/+server.ts
+++ src/routes/api/(shopDescriptions)/saveDesc/+server.ts
@@ -1,5 +1,4 @@
 import type { RequestHandler } from './$types';
-
 import { json } from '@sveltejs/kit'
 import type { validSession } from '$lib/types/user';
 import { validateBySessionId } from '$lib/helpers/user'
